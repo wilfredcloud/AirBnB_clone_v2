30d7098d208db67eb3fb37c9ece08de58459e31c
tonytec <tonyexplorer202@gmail.com>
2023-06-21 02:07:48 +0100

    Add setup_mysql_dev.sql file

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- create database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant user all privilege on database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- grant user select privilege on performance_schema db
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
