51f47307a8630d555ace60a8285b47277f7ffcf2
wilfredcloud <wilfredcloudspace@gmail.com>
2023-08-17 23:15:03 +0100

    feat: configure app to server content via /airbnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+# @app.route('/', strict_slashes=False)
+# def index():
+#     """returns Hello HBNB!"""
+#     return 'Hello HBNB!'
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
